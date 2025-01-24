e38b5351021f8c982f65bd3b58d0aa0c37d46ab6
sa.ka.wa <samiekaranjawainaina@gmail.com>
2025-01-24 12:09:00 +0300

    css update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,39 @@
-const genderButton = document.getElementById("genderButton");
+// const genderButton = document.getElementById("genderButton");
 
-document.getElementsByClassName(birthday).innerHTML = "Happy Birthday!";
+// document.getElementsByClassName(birthday).innerHTML = "Happy Birthday!";
 
-const cancelButton = document.getElementById("cancelButton");
+// const cancelButton = document.getElementById("cancelButton");
 
-cancelButton.addEventListener("click", function () {
-  console.log("Cancel button clicked");
-});
+// cancelButton.addEventListener("click", function () {
+//   console.log("Cancel button clicked");
+// });
+
+let male = document.getElementById("male");
+let female = document.getElementById("female");
+
+let dateInput = document.getElementById("dateInput");
+console.log(dateInput);
+
+function checkGender() {
+  if (male.checked) {
+    return {
+      Sunday: "Kwasi",
+      Monday: "Kwadwo",
+      Tuesday: "Kwabena",
+      Wednesday: "Kwaku",
+      Thursday: "Yaw",
+      Friday: "Kofi",
+      Saturday: "Kwame",
+    };
+  } else if (female.checked) {
+    return {
+      Sunday: "Akosua",
+      Monday: "Adwoa",
+      Tuesday: "Abenaa",
+      Wednesday: "Akua",
+      Thursday: "Yaa",
+      Friday: "Afua",
+      Saturday: "Ama",
+    };
+  }
+}
