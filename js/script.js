ed998689fc06ee18c16f215b3f63c1acae03fd6e
sa.ka.wa <samiekaranjawainaina@gmail.com>
2025-01-24 13:22:21 +0300

    restructuring the date

--- js/script.js
+++ js/script.js
@@ -8,32 +8,32 @@
 //   console.log("Cancel button clicked");
 // });
 
-let male = document.getElementById("male");
-let female = document.getElementById("female");
+// let male = document.getElementById("male");
+// let female = document.getElementById("female");
 
-let dateInput = document.getElementById("dateInput");
-console.log(dateInput);
+// let dateInput = document.getElementById("dateInput");
+// console.log(dateInput);
 
-function checkGender() {
-  if (male.checked) {
-    return {
-      Sunday: "Kwasi",
-      Monday: "Kwadwo",
-      Tuesday: "Kwabena",
-      Wednesday: "Kwaku",
-      Thursday: "Yaw",
-      Friday: "Kofi",
-      Saturday: "Kwame",
-    };
-  } else if (female.checked) {
-    return {
-      Sunday: "Akosua",
-      Monday: "Adwoa",
-      Tuesday: "Abenaa",
-      Wednesday: "Akua",
-      Thursday: "Yaa",
-      Friday: "Afua",
-      Saturday: "Ama",
-    };
-  }
-}
+// function checkGender() {
+//   if (male.checked) {
+//     return {
+//       Sunday: "Kwasi",
+//       Monday: "Kwadwo",
+//       Tuesday: "Kwabena",
+//       Wednesday: "Kwaku",
+//       Thursday: "Yaw",
+//       Friday: "Kofi",
+//       Saturday: "Kwame",
+//     };
+//   } else if (female.checked) {
+//     return {
+//       Sunday: "Akosua",
+//       Monday: "Adwoa",
+//       Tuesday: "Abenaa",
+//       Wednesday: "Akua",
+//       Thursday: "Yaa",
+//       Friday: "Afua",
+//       Saturday: "Ama",
+//     };
+//   }
+// }
